c0ba57ca19e9260e263fa55a5418c604858c7857
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-03-18 02:05:03 -0700

    mvp complete

---
+++ seeds/01-cars.js
@@ -0,0 +1,14 @@
+// exports.seed = function(knex) {
+//   const testData = [
+//     {vin: '01234567891234567', 'make'},
+//     {vin: '12345678901234567'},
+//     {vin: '23456789011234567'}
+//   ]
+
+//   // Deletes ALL existing entries
+//   return knex('table_name').del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('table_name').insert(testData);
+//     });
+// };
